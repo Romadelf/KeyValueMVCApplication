ffe13774c0827545f3dc9a4bfd8de27c4a88673c
Romadelf <romadelfatwork@hotmail.com>
2019-09-15 20:00:21 +0200

    Auto-generated test class

---
+++ src/test/java/be/technifutur/nrb/jasa/rdf/keyvaluemvcapplication/KeyValueApplicationTest.java
@@ -0,0 +1,14 @@
+package be.technifutur.nrb.jasa.rdf.keyvaluemvcapplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KeyValueApplicationTest {
+
+    @Test
+    void testMain() {
+	fail("Not yet implemented");
+    }
+
+}
